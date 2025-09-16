96f054a002fe4c5bc2e76ac53ba5027598bf3d1f
VISHAL-Nk <vishalnaik9239@gmail.com>
2025-09-16 10:11:08 +0530

    feat(api): implement GET route for products and search functionality with database connection management

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,3 +1,27 @@
-export default async function GET(){
-    
+
+import { dbConnect, dbDisconnect } from "@/lib/db";
+import Product from "@/models/Products.models";
+
+export default async function GET() {
+  try {
+    await dbConnect();
+
+    const products = Product.find({}).sort({ createdAt: -1 });
+    return new Response(JSON.stringify(products), { status: 200 });
+  } catch (error) {
+    let errorMessage = "An unknown error occurred.";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+      console.error("Products Route Error:", error.message, {
+        stack: error.stack,
+      });
+    }
+    return new Response(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+    });
+  }
+  finally{
+     await dbDisconnect(); 
+  }
 }
---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,32 @@
+import { dbConnect, dbDisconnect } from "@/lib/db";
+import Product from "@/models/Products.models";
+import { type NextRequest } from "next/server";
+
+export default async function GET(req: NextRequest) {
+  try {
+    const searchParams = new URL(req.url).searchParams;
+    const query = searchParams.get("q") || "";
+    await dbConnect();
+    const products = await Product.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { description: { $regex: query, $options: "i" } },
+      ],
+    }).sort({ createdAt: -1 });
+    return new Response(JSON.stringify(products), { status: 200 });
+  } catch (error) {
+    let errorMessage = "An unknown error occurred.";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+      console.error("Products Search Route Error:", error.message, {
+        stack: error.stack,
+      });
+    }
+    return new Response(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+    });
+  } finally {
+    await dbDisconnect();
+  }
+}
