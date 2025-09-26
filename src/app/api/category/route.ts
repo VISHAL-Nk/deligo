fe5e82b03deabb1c23cfb47448e7c30f2fc53f32
VISHAL-Nk <vishalnaik9239@gmail.com>
2025-09-26 16:24:18 +0530

    refactor(category): update GET route to use NextResponse and improve error handling

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,20 +1,23 @@
 import { dbConnect } from "@/lib/db";
-import { Session } from "@/lib/Session";
+// import { Session } from "@/lib/Session";
 import Category from "@/models/ProductCategories.models";
 import Product from "@/models/Products.models";
+import { NextResponse } from "next/server";
 
-export  async function GET(request: Request){
+export async function GET(request: Request){
     try{
-        const session = Session();
-        if(!session){
-            return new Response("Unauthorized", {status: 401});
-        }
+        // const session = Session();
+        // if(!session){
+        //     return new NextResponse("Unauthorized", {status: 401});
+        // }
 
         await dbConnect();
         const searchParams = new URL(request.url).searchParams;
         const category = searchParams.get("category");
         if(!category){
-            return new Response("Category is required", {status: 400});
+            const categories = await Category.find({status: "active"}).lean();
+            const top5Categories = categories.slice(0, 5);
+            return new NextResponse(JSON.stringify({categories,top5Categories}), {status: 200});
         }
         
         const categories = await Category.find({name: {$regex: category, $options: "i"}, status: "active"}).lean();
@@ -22,16 +25,16 @@ export  async function GET(request: Request){
         const product = await Product.find({categoryId: {$in: categories.map(cat => cat._id)}, status: "active"}).lean();
 
         if(!product || product.length === 0){
-            return new Response("No products found", {status: 404});
+            return new NextResponse("No products found", {status: 404});
         }
 
-        return new Response(JSON.stringify(product), {status: 200});
+        return new NextResponse(JSON.stringify(product), {status: 200});
     }
     catch(error){
         let message = "Internal Server Error";
         if(error instanceof Error){
             message = error.message;
         }
-        return new Response(message, {status: 500});
+        return new NextResponse(message, {status: 500});
     }
 }
