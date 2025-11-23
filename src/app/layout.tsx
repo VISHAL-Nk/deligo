13542838f43d8ec12ddbdfbcd1ed94a248530ab6
VISHAL-Nk <vishalnaik9239@gmail.com>
2025-11-23 20:02:53 +0530

    feat(analytics): add Vercel Analytics integration for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,13 @@ import "@/app/globals.css";
 import Providers from "@/components/Providers";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+  
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
               }}
             />
             {children}
+            <Analytics />
           </Providers>
         </ErrorBoundary>
       </body>
